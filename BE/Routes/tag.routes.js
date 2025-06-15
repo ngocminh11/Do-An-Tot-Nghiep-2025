d80b8dbea41b57104913b3976e2bbc4f9b1724f6
ngocminh11 <ngocminh110804@gmail.com>
2025-06-15 15:35:09 +0700

    Làm chức năng export danh sách sản phẩm thành file excel

--- BE/Routes/tag.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const tagController = require('../Controllers/tag.controller');
-
-// Public routes
-router.get('/', tagController.getAllTags);
-router.get('/:id', tagController.getTagById);
-
-module.exports = router; 
